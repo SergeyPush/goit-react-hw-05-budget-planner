75b536d8048abbd88fdc6352f754b8ac111dfa39
Sergey Pushkovskiy <push_s@mail.ua>
2019-11-28 22:58:36 +0200

    Updates

---
+++ precache-manifest.916f9b1b4cfc34fd57a1f4674a57fba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "549cb78f4abd9d607011",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime~main.0cda3cff.js"
+  },
+  {
+    "revision": "2f25883a7ad856038bac",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.65f58818.chunk.js"
+  },
+  {
+    "revision": "d815ced6d612e1e41104",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.c1adacb0.chunk.js"
+  },
+  {
+    "revision": "2f25883a7ad856038bac",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.25ea7c4b.chunk.css"
+  },
+  {
+    "revision": "05778cf7478358a7bdc4a90ed75e2007",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  }
+];
